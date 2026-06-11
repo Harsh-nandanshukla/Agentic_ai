c0cf27b776f801d276186df727766baf0ea6ebca
Harsh-nandanshukla <b23mt1019@iitj.ac.in>
2026-06-11 18:03:38 +0530

    Add LangSmith tutorial source files

---
+++ langsmith_tut/2_sequential_chain.py
@@ -0,0 +1,31 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+import os
+os.environ['LANGCHAIN_PROJECT']='seq_llm_app'
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template='Generate a detailed report on {topic}',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template='Generate a 5 pointer summary from the following text \n {text}',
+    input_variables=['text']
+)
+
+model1 = ChatOpenAI(model='gpt-4o-mini',temperature=0.7)
+model2=ChatOpenAI(model='gpt-4o',temperature=0.5)
+parser = StrOutputParser()
+config={
+    'tags':['llm_app','report_generation','summarization'],
+    'metadata':{'model1':'gpt-4o-mini','model1_temperature':0.7}
+}
+
+chain = prompt1 | model1 | parser | prompt2 | model2 | parser
+
+result = chain.invoke({'topic': 'Unemployment in India'},config=config)
+
+print(result)
